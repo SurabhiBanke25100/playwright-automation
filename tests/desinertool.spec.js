14df8d98b81d644d759067b53f5be1e927d46d94
SurabhiBanke25100 <surbhibanke2599@gmail.com>
2025-06-07 11:40:22 +0530

    first commit

---
+++ tests/desinertool.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test.use({
+  viewport: { width: 1920, height: 1080 }
+});
+
+test('interact with cross-origin iframe', async ({ page }) => {
+  await page.goto('https://gamiz-wp.designnbuy.biz/');
+
+  // Search for product
+  await page.locator('#dgwt-wcas-search-input-1').click();
+  await page.locator('#dgwt-wcas-search-input-1').fill('T-Shirt (QA)');
+  await page.locator('.dgwt-wcas-st-title', { hasText: 'T-Shirt (QA)' }).click();
+
+  // Click personalize
+  await page.getByRole('button', { name: /personalize/i }).click();
+
+  // Interact with iframe
+  const iframe = page.frameLocator('#designtool_iframe');
+
+  await iframe.locator('button#close').waitFor({ state: 'visible' });
+  await iframe.locator('button#close').click();
+
+ // await iframe.locator('//div[@class=".modal-header"]').waitFor({ state: 'visible' });
+  //const heading = await page.locator('h5.caption.text-uppercase');
+  //await heading.waitFor({state: 'visible'});
+
+ /* const headingText = await heading.textContent();
+  expect(headingText).toBe('Tool Panel');*/
+
+  await iframe.locator('div.modal-header:has-text("Tool Panel") > button.close').click();
+
+  await expect(iframe.locator('.dnbicon-type')).toBeVisible();
+  await iframe.locator('.dnbicon-type').click();
+
+  await expect(iframe.locator('span[data-lang-text="Heading"]')).toBeVisible();
+  await iframe.locator('span[data-lang-text="Heading"]').click();
+});
