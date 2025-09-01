14b99050f5681894ea1cae914129294de15027ab
Su25100 <surbhibanke2599@gmail.com>
2025-09-01 19:20:19 +0530

    reportportal configuration added

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-
+//import ReportPortal  from '@reportportal/agent-js-playwright';      // Import the ReportPortal plugin
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -23,8 +23,27 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ['@reportportal/agent-js-playwright', {  // Add ReportPortal reporter to Playwright config
+      endpoint: 'http://localhost:8080/api/v1',  // ReportPortal server URL (replace with your endpoint)
+      project: 'playwright_project', // Replace with your ReportPortal project name
+      apiKey: 'playwright-api__XMIfcJbSVe0J7q_PVkAuuNfF9uoUfCkgO2lsMBdtgxXepubqDjXYSgz8ynoqjJn',  // ReportPortal user token
+      launch: 'Playwright Test Run', // Launch name (can be dynamic)
+      description: 'Playwright tests integrated with ReportPortal',  // Optional: A description
+
+      //-----to display the steps in reportportal for passed testcases this attribut "includeTestSteps" is required to add in config file.------
+       includeTestSteps: true,
+      // 👇 ADD THIS LINE TO SHOW LOGS FOR PASSED TESTS
+     
+     // debug: true, // Optional: more detailed logs
+     // isLaunchMergeRequired: false,
+      reportSkippedTests: true,
+      //logTestSteps: true, 
+    }],
+    ['list'],  // Optional: still show test results in the terminal
+  ],
+
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
@@ -33,7 +52,7 @@ export default defineConfig({
     trace: 'on-first-retry',
      viewport: { width: 2000, height: 2000 },
   },
-
+   
   /* Configure projects for major browsers */
   projects: [
     {
